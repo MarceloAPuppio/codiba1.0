d335b53ff9fb5c20e889451d2d3f47735110da34
marceloapuppio <marcelopuppio@gmail.com>
2021-07-13 23:20:29 -0300

    Punto de inicio, desde versión 13/07/2021 web Codiba

---
+++ JS/Section.js
@@ -0,0 +1,30 @@
+document.querySelector("#btn-sales").addEventListener('click',function(){
+    document.querySelector("#section2bis").style.display="block";
+    document.querySelector("#section2bis").scrollIntoView();
+})
+document.querySelector("#btn-volver").addEventListener('click',function(){
+    document.querySelector("#section2bis").style.display="none";
+    document.querySelector("#section2").scrollIntoView();
+})
+document.querySelector("#btn-azucar").addEventListener('click',function(){
+    document.querySelector("#section2bisazucar").style.display="block";
+    document.querySelector("#section2bisazucar").scrollIntoView();
+})
+document.querySelector("#btn-volver-azucar").addEventListener('click',function(){
+    document.querySelector("#section2bisazucar").style.display="none";
+    document.querySelector("#section2").scrollIntoView();
+})
+document.querySelector("#btn-fecula").addEventListener('click',function(){
+    document.querySelector("#section2bisfecula").style.display="block";
+    document.querySelector("#section2bisfecula").scrollIntoView();
+})
+document.querySelector("#btn-volver-fecula").addEventListener('click',function(){
+    document.querySelector("#section2bisfecula").style.display="none";
+    document.querySelector("#section2").scrollIntoView();
+})
+document.getElementById("Form").addEventListener("submit",function(e){
+    e.preventDefault();
+    document.getElementById("Form").submit();
+    document.getElementById("Form").reset();
+    swal("Muchas Gracias", "Nos pondremos en contacto con usted a la brevedad", "success");
+})
---
+++ mailing.php
@@ -0,0 +1,32 @@
+<?php
+$nombre=$_POST['nombre'];
+$tel=$_POST['tel'];
+$mailto=$_POST['mailto'];
+$textarea=$_POST['textarea'];
+$asunto="Contacto vía Web Codiba";
+$desde = 'MIME-Version: 1.0' . "\r\n";
+$desde .= 'Content-type: text/html; charset=utf-8' . "\r\n";
+$desde .="From:"."noreply";
+$mensaje='<html>'.
+	'<head><title>Mensaje desde web de CODIBAL</title></head>'.
+	'<body style="background-color: rgba(178, 248, 213, 0.596);"><img src="https://lh3.googleusercontent.com/proxy/WrWMMWpcJusuC7Flz-_0HPARmAQ7KV_yzTfLDbMMpd1KncYZ7cLDut5wfYK1lAIodiYHlPVFHGdWrmW8QVTEaroS7R3Ca5s3c6HWR_cPlxz3wrkFjU0vAUJ246Fp" alt="www" style="height: 2.5rem;"><img src="https://fotos.subefotos.com/7b28565397c722c85654abde0cf5da81o.png" alt="Logo" style="height: 3rem;"><h2>Ha recibido un nuevo contacto desde la web</h2>'.
+    '<ul style="padding: 1rem;"><li><h3>Razón Social/ Nombre:<strong>'.
+         $nombre.
+	'<strong></h3></li><li><h3>Teléfono:<strong>'.
+    	 $tel.
+'<strong></h3></li><li><h3>Mail:<strong>'.
+$mailto.
+'<strong></h3></li></ul><h3>Motivo de consulta</h3><p style="text-align:justify; font-size: larger;">'.
+    	$textarea.
+'</p> <hr></body>'.
+
+	'</html>';
+mail('clientes@codiba.com',$asunto,$mensaje,$desde);
+?> 
+<html>
+<head>
+</head>
+<body>
+Mensaje Enviado
+</body>
+</html>
