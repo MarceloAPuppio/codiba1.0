5fd597eeb7c383070ab46c91e6852d44b4e453c3
marceloapuppio <marcelopuppio@gmail.com>
2021-07-15 02:10:32 -0300

    Captcha: se agregaron botones de refresh y speak, se hizo CSS

--- JS/Section.js
+++ JS/Section.js
@@ -1,47 +1,66 @@
-import {Captcha} from './Captcha.js'
-import {$} from './Tools.js'
-const captcha_visor=$('#captcha_visor')
-const captcha_text=$('#captcha_text')
+import { Captcha } from "./Captcha.js";
+import { $, talk } from "./Tools.js";
+const captcha_visor = $("#captcha_visor");
+const captcha_text = $("#captcha_text");
+const captcha_voiceBtn = $("#btn_voice");
+const captcha_refreshBtn = $("#btn_refresh");
+
 //Genero el Captcha aleatorio
-captcha_visor.innerHTML=Captcha.captchaGenerate();
+captcha_visor.innerHTML = Captcha.captchaGenerate();
 //Inicio la validación del captcha
-window.onload=()=>{
-Captcha.captchaValidate(captcha_visor,captcha_text)
-captcha_text.addEventListener('input',()=>{Captcha.captchaValidate(captcha_visor,captcha_text)})
-}
+window.onload = () => {
+  Captcha.captchaValidate(captcha_visor, captcha_text);
+  captcha_text.addEventListener("input", () => {
+    Captcha.captchaValidate(captcha_visor, captcha_text);
+  });
+};
+//Llamado a funcion Talk
+captcha_voiceBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  let text = captcha_visor.innerHTML.split("").join(",");
+  talk(text);
+});
+captcha_refreshBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  captcha_visor.innerHTML = Captcha.captchaGenerate();
+});
 
-
-
-document.querySelector("#btn-sales").addEventListener('click',function(){
-    document.querySelector("#section2bis").style.display="block";
-    document.querySelector("#section2bis").scrollIntoView();
-})
-document.querySelector("#btn-volver").addEventListener('click',function(){
-    document.querySelector("#section2bis").style.display="none";
+document.querySelector("#btn-sales").addEventListener("click", function () {
+  document.querySelector("#section2bis").style.display = "block";
+  document.querySelector("#section2bis").scrollIntoView();
+});
+document.querySelector("#btn-volver").addEventListener("click", function () {
+  document.querySelector("#section2bis").style.display = "none";
+  document.querySelector("#section2").scrollIntoView();
+});
+document.querySelector("#btn-azucar").addEventListener("click", function () {
+  document.querySelector("#section2bisazucar").style.display = "block";
+  document.querySelector("#section2bisazucar").scrollIntoView();
+});
+document
+  .querySelector("#btn-volver-azucar")
+  .addEventListener("click", function () {
+    document.querySelector("#section2bisazucar").style.display = "none";
     document.querySelector("#section2").scrollIntoView();
-})
-document.querySelector("#btn-azucar").addEventListener('click',function(){
-    document.querySelector("#section2bisazucar").style.display="block";
-    document.querySelector("#section2bisazucar").scrollIntoView();
-})
-document.querySelector("#btn-volver-azucar").addEventListener('click',function(){
-    document.querySelector("#section2bisazucar").style.display="none";
+  });
+document.querySelector("#btn-fecula").addEventListener("click", function () {
+  document.querySelector("#section2bisfecula").style.display = "block";
+  document.querySelector("#section2bisfecula").scrollIntoView();
+});
+document
+  .querySelector("#btn-volver-fecula")
+  .addEventListener("click", function () {
+    document.querySelector("#section2bisfecula").style.display = "none";
     document.querySelector("#section2").scrollIntoView();
-})
-document.querySelector("#btn-fecula").addEventListener('click',function(){
-    document.querySelector("#section2bisfecula").style.display="block";
-    document.querySelector("#section2bisfecula").scrollIntoView();
-})
-document.querySelector("#btn-volver-fecula").addEventListener('click',function(){
-    document.querySelector("#section2bisfecula").style.display="none";
-    document.querySelector("#section2").scrollIntoView();
-})
-document.getElementById("Form").addEventListener("submit",function(e){
-    e.preventDefault();
-    //descomentar cuando termine las pruebas
-    // document.getElementById("Form").submit();
-    // document.getElementById("Form").reset();
-    swal("Muchas Gracias", "Nos pondremos en contacto con usted a la brevedad", "success");
-    
-    
-})
+  });
+document.getElementById("Form").addEventListener("submit", function (e) {
+  e.preventDefault();
+  //descomentar cuando termine las pruebas
+  // document.getElementById("Form").submit();
+  // document.getElementById("Form").reset();
+  swal(
+    "Muchas Gracias",
+    "Nos pondremos en contacto con usted a la brevedad",
+    "success"
+  );
+});
--- JS/Tools.js
+++ JS/Tools.js
@@ -1,1 +1,13 @@
-export const $=(element)=>document.querySelector(element)
+//Selector
+export const $ = (element) => document.querySelector(element);
+//Fx talk
+export const talk = (text) => {
+  let speech = new SpeechSynthesisUtterance();
+  speech.lang = "es";
+  speech.text = text;
+  speech.volume = 1;
+  speech.rate = 1;
+  speech.pitch = 1;
+
+  window.speechSynthesis.speak(speech);
+};
